27e4c77907d7ea7fc8c9a8fdc7b10c7f9fa465f9
Alberto Cole <acpii2005@gmail.com>
2016-04-21 22:07:44 -0600

    Separating each module code to its own file

--- index.js
+++ index.js
@@ -1,36 +1,4 @@
-// Import the interface to Tessel hardware
-const tessel = require('tessel');
-const climatelib = require('climate-si7020');
-const climate = climatelib.use(tessel.port['A']);
-const pubnubSingleton = require('./util/pubnubSingleton');
-
-
-climate.on('ready', function () {
-  console.log('Connected to climate module');
-  // Loop forever
-  setImmediate(function loop () {
-    climate.readTemperature('c', (err, temp) => {
-      climate.readHumidity((err, humid) => {
-      // console.log(`humidity ${Math.round(humid)}`);
-      pubnubSingleton.publish(
-        'tent:climate',
-        {
-          temperature : Math.round(temp),
-          humidity    : Math.round(humid),
-        },
-        (callBackData) => {
-          console.log(callBackData);
-        },
-        (err) => {
-          console.log(JSON.stringify(err));
-        }
-      );
-      setTimeout(loop, 500);
-      });
-    });
-  });
-});
-
-climate.on('error', function(err) {
-  console.log('error connecting module', err);
-});
+const climateModule = require('./modules/climate');
+const relayModule = require('./modules/relay');
+climateModule.startReading();
+relayModule.startReading();
