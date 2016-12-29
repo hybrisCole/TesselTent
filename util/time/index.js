15cf88f439fefd130e09ee7aa96aaa3987222319
Alberto Cole <acpii2005@gmail.com>
2016-12-29 16:47:16 -0600

    update `currentTime` every 30 seconds

--- util/time/index.js
+++ util/time/index.js
@@ -2,19 +2,30 @@
 'use strict';
 const axios = require('axios');
 const moment = require('moment');
+const Rx = require('rxjs/Rx');
+const timeInterval = Rx.Observable.interval(1000 * 30);
 let currentTime = moment();
-// eslint-disable-next-line no-console
-console.time('reqDone');
-// This is needed bc we can't 100% rely on Tessel's internal time.
-exports.setup = function setup () {
+let internalStartDiff = moment();
+const requestTime = function requestTime () {
   axios.get('http://www.timeapi.org/utc/now.json').then((response) => {
     // eslint-disable-next-line no-console
-    console.timeEnd('reqDone');
     // Costa Rica time
     currentTime = moment(response.data.dateString).subtract(6, 'hours');
+    internalStartDiff = moment();
   }).catch((err) => {
     // eslint-disable-next-line no-console
     console.log(err);
+  });
+};
+// This is needed bc we can't 100% rely on Tessel's internal time.
+exports.setup = function setup () {
+  requestTime();
+  timeInterval.subscribe(() => {
+    const internalCurrentTime = moment();
+    const duration = moment.duration(internalCurrentTime.diff(internalStartDiff));
+    const elapsedSeconds = duration.asSeconds();
+    internalStartDiff = moment();
+    currentTime.add(elapsedSeconds, 's');
   });
 };
 
